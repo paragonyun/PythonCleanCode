083cac4a4639c13e098935fe1c0daa7e1139c830
paragonyun <paragonyun@naver.com>
2022-12-13 17:09:32 +0900

    __contains__

--- Chapter_2/Container.py
+++ Chapter_2/Container.py
@@ -11,13 +11,16 @@ def mark_coordinate(grid, coord):
     if 0 <= coord.x < grid.width and 0 <= coord.y < grid.hegith:
         grid[coord] = 1
 
+
 ## Good CAse
 class Boundaries:
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
-    def __contains__(self, coord): # __contains__를 통해 x와 y가 width와 height 안에 있는지 T/F로 return 한다.
+    def __contains__(
+        self, coord
+    ):  # __contains__를 통해 x와 y가 width와 height 안에 있는지 T/F로 return 한다.
         x, y = coord
         return 0 <= x < self.width and 0 <= y < self.height
 
@@ -26,10 +29,13 @@ class Grid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.limits = Boundaries(width, height) # 아까 만들어둔 class를 통해 width와 hegith 안에 corrd가 존재하는지 확인 가능
+        self.limits = Boundaries(
+            width, height
+        )  # 아까 만들어둔 class를 통해 width와 hegith 안에 corrd가 존재하는지 확인 가능
 
     def __contains__(self, coord):
         return coord in self.limits
+
 
 """두 class는 매우 응집력 있으며 최소한의 logic, 간결하고 자명한 메서드, 적절한 객체명을 가지고 있다. 매우 파이써닉!"""
 
@@ -38,4 +44,3 @@ class Grid:
 def mark_coordinate(grid, coord):
     if coord in grid:
         grid[coord] = 2
-
